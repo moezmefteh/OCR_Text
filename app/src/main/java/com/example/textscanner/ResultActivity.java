9f0c3f9c371fdbf33303686076050d84548eb42a
moez mefteh <moezmefteh85@gmail.com>
2021-11-15 13:34:17 +0100

    copy To ClipBoard

--- app/src/main/java/com/example/textscanner/ResultActivity.java
+++ app/src/main/java/com/example/textscanner/ResultActivity.java
@@ -2,9 +2,14 @@ package com.example.textscanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ResultActivity extends AppCompatActivity {
     CharSequence message;
@@ -18,6 +23,14 @@ public class ResultActivity extends AppCompatActivity {
         Intent intent = getIntent();
         message = intent.getStringExtra("detectedText");
         result.setText(message);
+
+    }
+
+    public void copyToClipBoard(View view) {
+        ClipboardManager clipBoard =(ClipboardManager)getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clipData=ClipData.newPlainText("copied data",message);
+        clipBoard.setPrimaryClip(clipData);
+        Toast.makeText(ResultActivity.this,"Copied to ClipBoard",Toast.LENGTH_LONG).show();
 
     }
 }
